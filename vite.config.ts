e1e5b417194ae676e2d90cc454bbf2379dd34a4b
Paul Guan <92047026+Misterfacile@users.noreply.github.com>
2026-02-13 11:35:20 +0100

    feat(deploy): Deploy the page

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
   plugins: [react()],
+  base: '/valentine-ask/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/"
-}));
+})
